3f0a7b166fdd995ce4a1ed2be26dfe1e554584d8
devumairazmat <umairazmat8260@gmail.com>
2022-07-12 07:08:38 +0500

    strings

--- script.js
+++ script.js
@@ -87,35 +87,33 @@ console.log(myDog); // Jack Russell Terrier
 
 let developer = "Dev Umair";
 console.log(developer);
-developer = "Dev Uamir Azmat" // can be redifine
+developer = "Dev Uamir Azmat"; // can be redifine
 console.log(developer);
 
 const dev = "dev marii";
 // dev= "Marii Dev"    error : can't be redefine
 console.log(dev);
 
-const stacks = ["Php" , "MERN"]
+const stacks = ["Php", "MERN"];
 console.log(stacks);
 stacks.push("Mean");
 console.log(stacks);
 
 const DevDetails = {
-    developer : "Umair",
-    location : "faisalabad",
-    level : "intermediate",
-    role : "frontend",
-    references : {
-     one: "nouman ul Haq",
-     two: "Prof Latif"
-    }
-}
+  developer: "Umair",
+  location: "faisalabad",
+  level: "intermediate",
+  role: "frontend",
+  references: {
+    one: "nouman ul Haq",
+    two: "Prof Latif",
+  },
+};
 Object.freeze(DevDetails);
 Object.freeze(DevDetails.references);
 console.log(DevDetails);
-DevDetails.Joining="2022";
+DevDetails.Joining = "2022";
 console.log(DevDetails);
-
-
 
 // TASK 3: Exercise: Refactor the code to use let/const
 
@@ -130,38 +128,37 @@ console.log(DevDetails);
 
 // solution
 var painter = {
-    name: "Van Gogh",
-    nationality: "Dutch",
-    paintings: ["The Starry Night", "Irises", "Almond Blossoms"]
+  name: "Van Gogh",
+  nationality: "Dutch",
+  paintings: ["The Starry Night", "Irises", "Almond Blossoms"],
 };
 Object.freeze(painter);
 Object.freeze(painter.paintings);
 //painter.birthDate = "March 30, 1853";
 //painter.paintings.push("Something");
 
- console.log(painter); // Should print { name: 'Van Gogh', nationality: 'Dutch', paintings: [ 'The Starry Night', 'Irises', 'Almond Blossoms' ] }
-
+console.log(painter); // Should print { name: 'Van Gogh', nationality: 'Dutch', paintings: [ 'The Starry Night', 'Irises', 'Almond Blossoms' ] }
 
 // ---------------------------------------------
 // TASK 4: Destructuring
 
 const academy = {
-    namee : "Learn with Umair",
-    since : 2002,
-    location :"Lahore",
-    Achievements : [ "1st in 2006" , "2nd i  2010"] ,
-}
+  namee: "Learn with Umair",
+  since: 2002,
+  location: "Lahore",
+  Achievements: ["1st in 2006", "2nd i  2010"],
+};
 // const namee = academy.namee;
 // const location = academy.location;
 
-const  { namee : academyName, location : academyLocation } = academy;
-console.log(academyName,academyLocation);
+const { namee: academyName, location: academyLocation } = academy;
+console.log(academyName, academyLocation);
 
-function courses({namee, location}) {
-    // console.log(academy.namee);
-    // console.log(academy.location);
-    console.log(namee);
-    console.log(location);
+function courses({ namee, location }) {
+  // console.log(academy.namee);
+  // console.log(academy.location);
+  console.log(namee);
+  console.log(location);
 }
 courses(academy);
 // console.log(academy);
@@ -193,40 +190,55 @@ courses(academy);
 
 // solution
 const ingredients = {
-    tea: 'black',
-    milk: 'soy',
-    sweetener: 'honey',
-    spices: ['ginger', 'cardamom', 'cinnamon', 'nutmeg']
-}
+  tea: "black",
+  milk: "soy",
+  sweetener: "honey",
+  spices: ["ginger", "cardamom", "cinnamon", "nutmeg"],
+};
 
 // Destructure the parameters
-function prepareChai({tea,spices,milk,sweetener} ) {
-   
-    console.log("Mix the " + tea + " tea " +
-        "with the " + spices + " in a small pot. " +
-        "Add a cup of water and bring to boil. Boil for 2-3 min. " +
-        "Add " + milk + " milk and " + sweetener + ". " +
-        "Simmer for 3 min. Serve masala chai hot or warm!");
+function prepareChai({ tea, spices, milk, sweetener }) {
+  console.log(
+    "Mix the " +
+      tea +
+      " tea " +
+      "with the " +
+      spices +
+      " in a small pot. " +
+      "Add a cup of water and bring to boil. Boil for 2-3 min. " +
+      "Add " +
+      milk +
+      " milk and " +
+      sweetener +
+      ". " +
+      "Simmer for 3 min. Serve masala chai hot or warm!"
+  );
 }
- prepareChai(ingredients);
-
+prepareChai(ingredients);
 
 //  Some plus
 
-const subjects = ["Programming","Database","DataStrucure","Algorithms"];
+const subjects = ["Programming", "Database", "DataStrucure", "Algorithms"];
 [first] = subjects;
-[,, third] = subjects;
-[first,second, ...restofSubjects] = subjects;
+[, , third] = subjects;
+[first, second, ...restofSubjects] = subjects;
 console.log(first);
 console.log(third);
 console.log(restofSubjects);
 
-
-
-
 // ---------------------------------------------
 // TASK 5: Strings and Interpolation
 
+const language = "Urdu";
+console.log(language);
+console.log(language.startsWith("U"));
+console.log(language.startsWith("R")); //false
+console.log(language.endsWith("u"));
+console.log(language.endsWith("r")); //false
+console.log(language.repeat(5));
+console.log(`The ${language} is a beautiful language`);
+
+// ---------------------------------------------
 // TASK 5: Exercise: Refactor the code to use the ES6 String utility methods
 
 // const country = "Bulgaria";
@@ -244,10 +256,23 @@ console.log(restofSubjects);
 //     console.log("The name ends with a");
 // }
 
-// console.log(
-//     "The capital of " + country +
-//     " is the city of " + city
-// );
+//solution
+const country = "Bulgaria";
+const city = "Sofia";
+
+if (country.indexOf("Bulg") > -1) {
+  console.log("The country starts with Bulg");
+}
+
+if (city.startsWith("So")) {
+  console.log("The name starts with So");
+}
+
+if (city.endsWith("a")) {
+  console.log("The name ends with a");
+}
+
+console.log(`The capital of ${country} is the city of " ${city} `);
 
 // ---------------------------------------------
 // TASK 6: Arrow functions
